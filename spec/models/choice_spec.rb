9301f220874295079052ffd34a18d91ff8bc3d6d
h-i-k-e-n <75025826+h-i-k-e-n@users.noreply.github.com>
2021-02-24 22:12:11 +0900

    choice.modelテストコード完了

--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 RSpec.describe Choice, type: :model do
   describe '#create' do
     before do
+      
       @check = FactoryBot.create(:check)
-      @choice = FactoryBot.build(:choice)
-      sleep 1
+      @choice = FactoryBot.build(:choice, check_id: @check.id)
     end
 
-context 'チェックリストを登録できるとき' do
+    context 'チェックリストを登録できるとき' do
 
-    it "checkが存在したら投稿できる" do 
-      expect(@choice).to be_valid     
+        it "nameが存在したら投稿できる" do 
+          expect(@choice).to be_valid     
+        end
     end
 
+    context 'チェックリストを登録できないとき' do
+
+        it "nameが存在しないとき保存できないこと"do
+              @choice.name = nil
+              @choice.valid?
+              expect(@choice.errors.full_messages).to include("Nameを入力してください")
+        end
+     end
+end
 end
 
-end
-end
+    
